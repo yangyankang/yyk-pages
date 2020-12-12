7740a8cef36bc950e5150b99d67c54fa63031a8f
杨焱康 <704799639@qq.com>
2020-12-12 18:59:50 +0800

    finish

--- bin/yyk-pages.js
+++ bin/yyk-pages.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
+process.argv.push('--cwd')
+process.argv.push(process.cwd())
+process.argv.push('--gulpfile')
+process.argv.push(require.resolve('..'))
 
-console.log('zce=page')
+require('gulp/bin/gulp')
+
+
